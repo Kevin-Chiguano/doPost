678a20c5455ae3a6a95c283bcd8e3a10525bfd6a
Kevin <kevin64dj@gmail.com>
2024-05-20 09:51:00 -0500

    Commint final

--- src/main/java/org/kevin/servlet/parametros/EstudianteServlet.java
+++ src/main/java/org/kevin/servlet/parametros/EstudianteServlet.java
@@ -35,10 +35,26 @@ public class EstudianteServlet extends HttpServlet {
         out.println("<meta charset=\"UTF-8\">");
         out.println("<title>Datos del Estudiante</title>");
         out.println("<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" rel=\"stylesheet\">");
+        out.println("<style>");
+        out.println("  .container {");
+        out.println("      width: 40%;");
+        out.println("      margin: 0 auto;");
+        out.println("      padding: 20px;");
+        out.println("      border: 1px solid #ccc;");
+        out.println("      border-radius: 10px;");
+        out.println("      background-color: #f9f9f9;");
+        out.println("  }");
+        out.println("  body {");
+        out.println("      background: url('https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHRtbHxlbnwwfHwwfHx8MA%3D%3D');");
+        out.println("      background-size: cover;");
+        out.println("      background-repeat: no-repeat;");
+        out.println("      display: grid;");
+        out.println("  }");
+        out.println("</style>");
         out.println("</head>");
         out.println("<body>");
         out.println("<div class=\"container mt-5\">");
-        out.println("<h1>Datos del Estudiante</h1>");
+        out.println("<h1>Datos Personales</h1>");
         out.println("<p><strong>Nombre:</strong> " + nombre + "</p>");
         out.println("<p><strong>Apellido:</strong> " + apellido + "</p>");
         out.println("<p><strong>Edad:</strong> " + edad + "</p>");
@@ -49,5 +65,7 @@ public class EstudianteServlet extends HttpServlet {
         out.println("</div>");
         out.println("</body>");
         out.println("</html>");
+
     }
+
 }
